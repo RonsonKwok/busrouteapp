fe32045d049e55d40834a6c06fac82bda6bafacc
Ronson Kwok <rk6866@hotmail.com>
2023-03-10 16:17:23 +0800

    Amended and success on getting response from api

--- lib/busroute/repositories/bus_route_repo.dart
+++ lib/busroute/repositories/bus_route_repo.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'package:busrouteapp/busroute/models/bus_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class BusRouteRepositoryException implements Exception {}
 
 class BusRouteRepository {
   final http.Client _httpClient;
-  final _baseUrl = 'data.etabus.gov.hk/v1/transport/kmb/route/';
+  final _baseUrl = 'data.etabus.gov.hk';
 
   BusRouteRepository({http.Client? httpClient})
       : _httpClient = httpClient ?? http.Client();
@@ -14,14 +15,17 @@ class BusRouteRepository {
   Future<List<Data>> getBusRouteItems() async {
     var busRouteRequest = Uri.https(
       _baseUrl,
+      'v1/transport/kmb/route/',
     );
     var busRouteResponse = await _httpClient.get(busRouteRequest);
+    print(busRouteResponse);
 
     if (busRouteResponse.statusCode != 200) {
       throw BusRouteRepositoryException();
     }
     var busRouteJson = json.decode(busRouteResponse.body);
     final List<dynamic> results = busRouteJson['data'];
+    print(results);
     return results.map((json) => Data.fromJson(json)).toList();
   }
 }
